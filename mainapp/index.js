a08342af40c686e31cb0ad1e3344542fca549c39
domalee <136012609+domalee@users.noreply.github.com>
2023-06-28 14:45:24 -0700

    Add files via upload

---
+++ mainapp/index.js
@@ -0,0 +1,13 @@
+var dependency = require('./../lib/dependency.js');
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  var number = dependency.getNumber();
+  res.send('This is the number: ' + number);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
