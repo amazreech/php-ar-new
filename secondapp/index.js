36ecada6ffcaa5a52ae3eed457160ac575d86a7a
domalee <136012609+domalee@users.noreply.github.com>
2023-08-11 12:09:01 -0700

    Update index.js

--- secondapp/index.js
+++ secondapp/index.js
@@ -1,9 +1,10 @@
+var dependency = require('./../lib/dependency.js');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Second App');
+    res.send('[SECOND APP] This is the number from the library folder: ' + number);
 });
 
 app.listen(port, () => {
